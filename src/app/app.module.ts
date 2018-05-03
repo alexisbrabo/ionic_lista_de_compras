093495381b7b30e10a275a6a18322fbefdf66827
alexisbrabo <alexis_brabo@hotmail.com>
2018-05-03 17:06:12 -0300

    First Comiit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,31 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component';  
+import { FIREBASE_CONFIG } from './firebase.credentials';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { ShoppingListService } from '../services/shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ShoppingListService
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB0iyYZe_4W2Kfq7YrbLwOYhaGP-3_eDZ8",
+    authDomain: "shoppinglist-e126a.firebaseapp.com",
+    databaseURL: "https://shoppinglist-e126a.firebaseio.com",
+    projectId: "shoppinglist-e126a",
+    storageBucket: "shoppinglist-e126a.appspot.com",
+    messagingSenderId: "106789222817"
+};
